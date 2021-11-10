f18b3a8df693c1f32d97e1f00da7d30203f8583b
gurami98 <chilindrishviligurami@gmail.com>
2021-11-10 22:40:13 +0400

    configure json-server authentication middleware

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
+server.db = router.db
 
-server.use(middlewares);
+server.use(auth);
 server.use(router);
 
 server.listen(port);
